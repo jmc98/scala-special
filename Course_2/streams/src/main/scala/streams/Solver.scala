e5180df0fe13699edf3eba3c3ce84372406cf5da
Michael Camacho <code@nabudis.com>
2020-06-16 18:33:12 +0100

    initial explored states should include starting state

--- Course_2/streams/src/main/scala/streams/Solver.scala
+++ Course_2/streams/src/main/scala/streams/Solver.scala
@@ -87,7 +87,7 @@ trait Solver extends GameDef {
    * The stream of all paths that begin at the starting block.
    */
   lazy val pathsFromStart: Stream[(Block, List[Move])] =
-    from(initial = Stream((startBlock, Nil)), explored = Set())
+    from(initial = Stream((startBlock, Nil)), explored = Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
